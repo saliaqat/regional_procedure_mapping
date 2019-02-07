bb2ed40cbcb8254d7eb8099194043bacaf3a8ca8
Salaar <salaar.rox@hotmail.com>
2019-02-07 18:16:20 -0500

    fixed code in data manipulator

--- data_manipulator.py
+++ data_manipulator.py
@@ -1,9 +1,19 @@
 from sklearn.model_selection import train_test_split
 
+all_columns = ['RIS PROCEDURE CODE', 'RIS PROCEDURE DESCRIPTION',
+       'PACS SITE PROCEDURE CODE', 'PACS PROCEDURE DESCRIPTION',
+       'PACS STUDY DESCRIPTION', 'PACS BODY PART', 'PACS MODALITY',
+       'DEFAULT LOCALIZATION FOR FEM', 'ON WG IDENTIFIER']
+feature_columns = ['RIS PROCEDURE CODE', 'RIS PROCEDURE DESCRIPTION',
+       'PACS SITE PROCEDURE CODE', 'PACS PROCEDURE DESCRIPTION',
+       'PACS STUDY DESCRIPTION', 'PACS BODY PART', 'PACS MODALITY',
+       'DEFAULT LOCALIZATION FOR FEM']
+label_columns = ['ON WG IDENTIFIER']
 
-def get_train_test_split(self, random_state=1337, label='ON WG IDENTIFIER', test_size=0.25):
-    # src_file is not a feature
-    df = self.df.drop('src_file', axis=1)
+
+def get_train_test_split(input_df, random_state=1337, label=label_columns, test_size=0.25):
+    # dropping src_file since its is not a feature
+    df = input_df.drop('src_file', axis=1)
 
     train, test = train_test_split(df, random_state=random_state, test_size=test_size, shuffle=True)
 
@@ -15,10 +25,10 @@ def get_train_test_split(self, random_state=1337, label='ON WG IDENTIFIER', test
     return train_x, train_y, test_x, test_y
 
 
-def get_train_validate_test_split(self, random_state=1337, label='ON WG IDENTIFIER', test_size=0.125,
+def get_train_validate_test_split(input_df, random_state=1337, label=label_columns, test_size=0.125,
                                   validation_split=0.125):
-    # src_file is not a feature
-    df = self.df.drop('src_file', axis=1)
+    # dropping src_file since its is not a feature
+    df = input_df.drop('src_file', axis=1)
 
     first_split = test_size + validation_split
     second_split = validation_split / first_split
@@ -35,3 +45,7 @@ def get_train_validate_test_split(self, random_state=1337, label='ON WG IDENTIFI
     test_y = test[label]
 
     return train_x, train_y, validation_x, validation_y, test_x, test_y
+
+
+def get_word_frequency_data(x, y):
+    pass
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from Models.simple_logistic_regression import LogisticRegressionModel
 from Models.model import Model
 from data_reader import DataReader
+from data_manipulator import *
 
 def main():
-    lg = LogisticRegressionModel()
+    # Sample code to read and split data
 
-    train_x, train_y, test_x, test_y = get_sample_data()
+    # data_reader = DataReader()
+    # df = data_reader.get_all_data()
+    # train_x, train_y, test_x, test_y = get_train_test_split(df)
 
-    lg.train(train_x, train_y)
-    evaluate_model(lg,test_x ,test_y , plot_roc=True)
-    
+
+    # Sample code to train logistic regression model
+
+    # lg = LogisticRegressionModel()
+    #
+    # train_x, train_y, test_x, test_y = get_sample_data()
+    #
+    # lg.train(train_x, train_y)
+    # evaluate_model(lg,test_x ,test_y , plot_roc=True)
+
+    # Opens interactive terminal. Used to try out code.
+    from IPython import embed
+    embed()
+
+    pass
+
 
 def get_sample_data():
     train_x = [[1], [2], [3], [4], [5]]
@@ -22,9 +38,11 @@ def evaluate_model(model, test_x, test_y, plot_roc=False):
     predictions = model.predict(test_x)
     score = model.score(test_x, test_y)
     AUC = model.AUC(test_x, test_y)
+    F1 = model.F1(test_x, test_y)
+
     if plot_roc:
         model.plot_ROC(test_x, test_y)
-    F1 = model.F1(test_x, test_y)
+
     print(model.get_name() + ' Evaluation')
     print("predictions: ", predictions)
     print("score: ", score)
