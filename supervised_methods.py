9eb82c9d355d133edb218892481711c4ae53b3e8
Salaar <salaar.rox@hotmail.com>
2019-02-10 14:57:32 -0500

    added cache_em_all library to supervised_methods. Used to save versions of models so retraining isn't nessessary

--- supervised_methods.py
+++ supervised_methods.py
@@ -3,18 +3,21 @@ from Models.model import Model
 from data_reader import DataReader
 from data_manipulator import *
 
+from cache_em_all import Cachable
+
 # What Salaar is working on.
 
 def main():
     supervised_scratch()
 
 def supervised_scratch():
+    multiclass_logistic_regression()
     pass
 
 # Baseline
 # Score:    0.7962874821513565
 # F1:       0.7962874821513564
-def multclass_logistic_regression():
+def multiclass_logistic_regression():
     data_reader = DataReader()
     df = data_reader.get_all_data()
     train_x_raw, train_y_raw, test_x_raw, test_y_raw = get_train_test_split(df)
@@ -22,13 +25,19 @@ def multclass_logistic_regression():
     tokens, train_y_raw = tokenize(train_x_raw, train_y_raw, save_missing_feature_as_string=True)
     train_x, train_y, feature_names = tokens_to_features(tokens, train_y_raw)
 
+    lg = _get_multiclass_logistic_regression_model(train_x, train_y)
+
     tokens, test_y_raw = tokenize(test_x_raw, test_y_raw, save_missing_feature_as_string=True)
     test_x, test_y, _ = tokens_to_features(tokens, test_y_raw, feature_names=feature_names)
 
+    evaluate_model(lg, test_x, test_y, plot_roc=False)
+
+# Cacheable saves the model, so we don't have to train it again. (training takes around 15 minutes)
+@Cachable("multiclass_logistic_regression_model.pkl", version=1)
+def _get_multiclass_logistic_regression_model(train_x, train_y):
     lg = MultiClassLogisticRegression()
     lg.train(train_x, train_y)
-
-    evaluate_model(lg, test_x, test_y, plot_roc=False)
+    return lg
 
 def evaluate_model(model, test_x, test_y, plot_roc=False):
     predictions = model.predict(test_x)
