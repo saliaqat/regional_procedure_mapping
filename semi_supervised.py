4e7e4e04dee8d53bbe2083ba085692308f24bf6a
Jeevaa Velayutham <32050253+jeevaav@users.noreply.github.com>
2019-02-14 17:07:14 -0500

    semi supervised file edited

--- semi_supervised.py
+++ semi_supervised.py
@@ -29,41 +29,41 @@ from data_manipulator import *
 import warnings
 warnings.filterwarnings("ignore")
 
-# get all the labelled data
-reader = DataReader()
-data = reader.get_all_data()
-
-print('\n================== Before removing missing values ==================\n')
-print('No. of samples: {}'.format(len(data)))
-print('No. of classes: {}'.format(data['ON WG IDENTIFIER'].nunique()))
-counts = data.groupby(['ON WG IDENTIFIER']).size().to_frame(name='counts') \
-                        .sort_values(['counts']).values
-print('Max no. of samples for a class: {}'.format(counts[-1][-1]))
-print('Min no. of samples for a class: {}'.format(counts[0][0]))
-print('Avg no. of samples for a class: {}'.format(round(np.mean(counts), 2)))
-print('\n===================================================================\n')
-
-print('================== After removing missing values ====================\n')
-# drop rows with missing values
-dataNoNan = data.dropna()
-print('No. of samples: {}'.format(len(dataNoNan)))
-print('No. of classes: {}'
-      .format(dataNoNan['ON WG IDENTIFIER'].nunique()))
-
-counts = dataNoNan.groupby(['ON WG IDENTIFIER']).size().to_frame(name='counts') \
-                        .sort_values(['counts']).values
-print('Max no. of samples for a class: {}'.format(counts[-1][-1]))
-print('Min no. of samples for a class: {}'.format(counts[0][0]))
-print('Avg no. of samples for a class: {}'.format(round(np.mean(counts), 2)))
-print('\n====================================================================')
-
-
-data = data.drop(['src_file'], axis=1)
-targets = data['ON WG IDENTIFIER'].values
-old_features = data.drop(['ON WG IDENTIFIER'], axis=1).values
-features = []
-for i in range(len(old_features)):
-    features.append(' '.join(str(i) for i in old_features[i]))
+## get all the labelled data
+#reader = DataReader()
+#data = reader.get_all_data()
+#
+#print('\n================== Before removing missing values ==================\n')
+#print('No. of samples: {}'.format(len(data)))
+#print('No. of classes: {}'.format(data['ON WG IDENTIFIER'].nunique()))
+#counts = data.groupby(['ON WG IDENTIFIER']).size().to_frame(name='counts') \
+#                        .sort_values(['counts']).values
+#print('Max no. of samples for a class: {}'.format(counts[-1][-1]))
+#print('Min no. of samples for a class: {}'.format(counts[0][0]))
+#print('Avg no. of samples for a class: {}'.format(round(np.mean(counts), 2)))
+#print('\n===================================================================\n')
+#
+#print('================== After removing missing values ====================\n')
+## drop rows with missing values
+#dataNoNan = data.dropna()
+#print('No. of samples: {}'.format(len(dataNoNan)))
+#print('No. of classes: {}'
+#      .format(dataNoNan['ON WG IDENTIFIER'].nunique()))
+#
+#counts = dataNoNan.groupby(['ON WG IDENTIFIER']).size().to_frame(name='counts') \
+#                        .sort_values(['counts']).values
+#print('Max no. of samples for a class: {}'.format(counts[-1][-1]))
+#print('Min no. of samples for a class: {}'.format(counts[0][0]))
+#print('Avg no. of samples for a class: {}'.format(round(np.mean(counts), 2)))
+#print('\n====================================================================')
+#
+#
+#data = data.drop(['src_file'], axis=1)
+#targets = data['ON WG IDENTIFIER'].values
+#old_features = data.drop(['ON WG IDENTIFIER'], axis=1).values
+#features = []
+#for i in range(len(old_features)):
+#    features.append(' '.join(str(i) for i in old_features[i]))
 
 print('Fitting logistic regression with original features ...')
 data_reader = DataReader()
