9b737d0609144b406506c27c9d32b0cc336e51e6
Jeevaa Velayutham <jeevaa.velayutham@mail.utoronto.ca>
2019-03-20 14:40:28 -0400

    siamese nn updated

--- semi_supervised.py
+++ semi_supervised.py
@@ -39,10 +39,10 @@ data_reader = DataReader()
 df = data_reader.get_all_data()
 train_x_raw, train_y_raw, test_x_raw, test_y_raw = get_train_test_split(df)
 
-tokens, train_y_raw = tokenize(train_x_raw, train_y_raw, save_missing_feature_as_string=True)
+tokens, train_y_raw = tokenize_columns(train_x_raw, train_y_raw, save_missing_feature_as_string=True)
 train_x, train_y, feature_names = tokens_to_bagofwords(tokens, train_y_raw)
 
-tokens, test_y_raw = tokenize(test_x_raw, test_y_raw, save_missing_feature_as_string=True)
+tokens, test_y_raw = tokenize_columns(test_x_raw, test_y_raw, save_missing_feature_as_string=True)
 test_x, test_y, _ = tokens_to_bagofwords(tokens, test_y_raw, feature_names=feature_names)
 
 # encode the labels into smaller integers rather than large integers
@@ -55,13 +55,12 @@ test_y = le.transform(test_y.values)
 x = np.concatenate((train_x.todense(), test_x.todense()))
 y = np.concatenate((train_y, test_y))
 
-# reduce the dimension using autoencoder
-#encoder = load_model('encoder-1000.h5')
-#x = encoder.predict(x)
+# no. of classes
+limit = 1000
 
 # this function creates pair between same class and different class with
 # appropriate targets    
-def create_pair(labels_dict):
+def create_pair(labels_dict, labels):
     
     dataset_sim = []
     targets_sim = []
@@ -71,26 +70,27 @@ def create_pair(labels_dict):
             for j in range(len(samples)):
                 dataset_sim.append([samples[i], samples[j]])
                 targets_sim.append(1)
-
+    
     dataset_diff = []
     targets_diff = []
-    for labelA in sorted(labels_dict):
-        for labelB in sorted(labels_dict):
-            if labelA != labelB:
-                samplesA = labels_dict[labelA]
-                samplesB = labels_dict[labelB]
-                for i in range(len(samplesA)):
-                    for j in range(len(samplesB)):
-                        dataset_diff.append([samplesA[i], samplesB[j]])
-                        targets_diff.append(0)
-                        
-    idx = np.random.permutation(len(dataset_sim))    
-    return dataset_sim + list(np.array(dataset_diff)[idx]), \
-                    targets_sim + list(np.array(targets_diff)[idx])
-                                         
     
+    while len(targets_diff) != len(targets_sim):
+        idx1 = np.random.randint(len(labels))
+        idx2 = np.random.randint(len(labels))
+        if idx1 != idx2:
+            samplesA = labels_dict[labels[idx1]]
+            samplesB = labels_dict[labels[idx2]]
+            a = np.random.randint(len(samplesA))
+            b = np.random.randint(len(samplesB))
+            dataset_diff.append([samplesA[a], samplesB[b]])
+            targets_diff.append(0)
+            
+            
+    return dataset_sim +dataset_diff, \
+                    targets_sim + targets_diff
+
 # create dataset for siamese neural network
-def create_pairwise_dataset(x, y, k=8, train_ratio=0.6, limit=50):
+def create_pairwise_dataset(x, y, k=15, train_ratio=0.66, limit=limit):
     # k is the minimum sample needed for each class
     # train ratio says split the data into 80:20
         
@@ -105,6 +105,7 @@ def create_pairwise_dataset(x, y, k=8, train_ratio=0.6, limit=50):
 
     labels_dict_train = {}
     labels_dict_test = {}
+    labels = []
     counter = 1
     for label in sorted(labels_dict):
         if counter > limit:
@@ -112,27 +113,29 @@ def create_pairwise_dataset(x, y, k=8, train_ratio=0.6, limit=50):
         if len(labels_dict[label]) >= k:
             labels_dict_train[label] = labels_dict[label][:int(k*train_ratio)]
             labels_dict_test[label] = labels_dict[label][int(k*train_ratio):k]
-            
+            labels.append(label)
         counter += 1
-         
+            
     # delete the other samples 
     del labels_dict
     
     print('Creating dataset with total classess of {} ..'.format(len(labels_dict_train)))
         
-    train_x, train_y = create_pair(labels_dict_train)
-    
+    train_x, train_y = create_pair(labels_dict_train, labels)
+
     print('Total samples created for training: {}'.format(len(train_y)))
     
-    test_x, test_y = create_pair(labels_dict_test)
+    test_x, test_y = create_pair(labels_dict_test, labels)
     
     print('Total samples created for testing: {}'.format(len(test_y)))
     
-    return train_x, test_x, train_y, test_y
+    return train_x, test_x, train_y, test_y, labels_dict_train, labels_dict_test
 
 train_x, test_x, train_y, test_y = 0, 0, 0, 0
 
-train_paired, test_paired, train_paired_target, test_paired_target = create_pairwise_dataset(x, y)
+train_paired, test_paired, \
+train_paired_target, test_paired_target, \
+labels_dict_train, labels_dict_test = create_pairwise_dataset(x, y)
 
 # set the data as separate numpy array
 pair1_train = []
@@ -161,10 +164,15 @@ test_paired_target = np.array(test_paired_target)
 # shuffle in unison
 pair1_test, pair2_test, test_paired_target = unison_shuffled_copies(pair1_test, pair2_test, test_paired_target)
  
-pair1_train = pair1_train.reshape(-1, 24472)
-pair2_train = pair2_train.reshape(-1, 24472)  
-pair1_test = pair1_train.reshape(-1, 24472)
-pair2_test = pair2_train.reshape(-1, 24472)   
+#pair1_train = pair1_train.reshape(-1, 24472)
+#pair2_train = pair2_train.reshape(-1, 24472)  
+#pair1_test = pair1_test.reshape(-1, 24472)
+#pair2_test = pair2_test.reshape(-1, 24472) 
+
+pair1_train = pair1_train.reshape(-1, 5592)
+pair2_train = pair2_train.reshape(-1, 5592)  
+pair1_test = pair1_test.reshape(-1, 5592)
+pair2_test = pair2_test.reshape(-1, 5592)   
 
 #np.save(pair1_train)
 #np.save(pair2_train)
@@ -173,39 +181,93 @@ pair2_test = pair2_train.reshape(-1, 24472)
 #np.save(train_paired_target)
 #np.save(test_paired_target)
 
+# cosine similarity distance metric
+def cosine_distance(x, y):
+    x = K.l2_normalize(x, axis=-1)
+    y = K.l2_normalize(y, axis=-1)
+    return -K.mean(x * y, axis=-1, keepdims=True)
+
+def jaccard_distance(y_true, y_pred, smooth=100):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1, keepdims=True)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1, keepdims=True)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+def euclidean_distance(y_true, y_pred):
+    return K.mean(K.sum(K.square(y_true - y_pred)))
+
+
 # siamese neural network structure 
 input_shape = pair1_train.shape[1]
 left_input = Input(shape=(input_shape,))
 right_input = Input(shape=(input_shape,))
 
 nnet = Sequential()
-nnet.add(Dense(500, activation='relu', input_dim=input_shape))
-nnet.add(Dense(250, activation='relu'))
+nnet.add(Dense(250, activation='relu', input_dim=input_shape))
+#nnet.add(Dense(1000, activation='relu'))
 nnet.add(Dense(100, activation='relu'))
-nnet.add(Dense(50, activation="sigmoid"))
+nnet.add(Dense(50, activation='relu'))
 
 #encode each of the two inputs into a vector with the convnet
 encoded_l = nnet(left_input)
 encoded_r = nnet(right_input)
 
 #merge two encoded inputs with the l1 distance between them
-l1_distance = lambda x: K.abs(x[0]-x[1])
-merge = Lambda(l1_distance)([encoded_l, encoded_r])
-merge1 = Dense(1,activation='sigmoid')(merge)
-siamese_net = Model(input=[left_input,right_input],output=merge1)
-siamese_net.compile(loss="binary_crossentropy",optimizer='adam', metrics=['accuracy'])
+distance = lambda x: K.abs(x[0] - x[1])
+merge = Lambda(distance)([encoded_l, encoded_r])
+merge = Dense(1, activation='sigmoid')(merge)
+siamese_net = Model(input=[left_input,right_input],output=merge)
+siamese_net.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(siamese_net.summary())
 
 siamese_net.fit([pair1_train, pair2_train], train_paired_target, 
                 validation_data=([pair1_test, pair2_test], test_paired_target),
-                epochs=100, batch_size=500, shuffle=True)
+                epochs=10, batch_size=500, shuffle=True)
 
 siamese_net.save('siamese.h5')
 
-# evaluation for siamese network few-shot learning technique
-#for i in range(len(pair1_test)):
-#    outputs = []
-#    for j in range(len(pair2_test))
-#    pair1_test[i]
+#siamese_net = load_model('siamese.h5')
 
+#================== evaluation for siamese network ========================#
 
+# get the support vectors set
+support_set = {}
+counter = 1
+for labelA in sorted(labels_dict_train):
+    if counter > limit:
+        break
+    support_set[labelA] = labels_dict_train[labelA]
+    counter += 1
+
+# get samples to be tested
+test_set = {} 
+counter = 1
+for labelA in sorted(labels_dict_test):
+    if counter > limit:
+        break
+    samples = labels_dict_test[labelA]
+    idx = np.random.randint(len(samples))
+    test_set[labelA] = samples[idx]
+    counter += 1  
+    
+# run testing in a non-parametric way    
+correct = 0
+total = 0
+for t in test_set:
+    label = -1
+    max_dist = -1
+    for s in support_set:
+        samples = support_set[s]
+        d = 0
+        for sample in samples:
+            d += siamese_net.predict([sample, test_set[t]])
+        distance = d / len(samples)
+        if distance[-1][-1] > max_dist:
+            max_dist = distance[-1][-1]
+            label = s
+    
+    if t == label:
+        correct += 1
+    total += 1
+        
+print('Accuracy: ', correct/total)
