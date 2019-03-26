edbbc75d2e51b24e73da7bca60e34b7762c9b501
jopineda <joannaa.pineda@gmail.com>
2019-03-26 15:32:33 -0400

    main script for running unsupervised models

--- unsupervised_methods.py
+++ unsupervised_methods.py
@@ -20,34 +20,12 @@ from keras.models import Model
 from keras.optimizers import RMSprop
 from Models.lda import Lda
 from Models.kmeans import Kmeans
+from Models.dbscan import DBscan
+from Models.birch import Birch_
+from Models.hierarchial import Hierarchial
 
-def run_models(train_x, train_y, feature_names):
-    # run clustering
-    num_clusters = 1500
-    cluster_full_set(num_clusters, train_x, train_y, feature_names)
 
-def cluster_full_set(num_clusters,train_x, train_y, feature_names):
-    nc = num_clusters
-
-    kmeans = Kmeans(n_clusters=nc, random_state=0).fit(train_x)
-    labels = kmeans.labels_
-
-    cluster = Cluster(kmeans, feature_names, train_x, labels)
-    
-    #print("getting nearest")
-    #cluster.get_nearest_neighbours("Y DIR - ANGIOGRAM")
-
-    sil_score = metrics.silhouette_score(train_x, labels, metric='euclidean')
-    db_idx_score = metrics.davies_bouldin_score(train_x, labels)
-    #scores.append(sil_score)
-    #label = s.lstrip("input_data/").rstrip(".csv")
-    #    x_labels.append(label)
-    #    dataset_sizes.append(train_x.shape[0])
-    print(sil_score)
-    print(db_idx_score)
-    #plot_sil_scores_per_site(scores, x_labels)
-    #plot_dataset_size_per_site(dataset_sizes, x_labels)
-
+import argparse
 
 def plot_dataset_size_per_site(dataset_sizes, x_labels):
     fig = plt.figure()
@@ -67,48 +45,72 @@ def plot_sil_scores_per_site(scores, x_labels):
     plt.title('Silhouette Scores for Clustering by Site')
     plt.savefig("sil_scores_per_site.pdf", bbox_inches = "tight")
 
+
 def main():
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Run unsupervised methods', add_help=False)
+    parser.add_argument("-h", "--help",  action="store_true", dest="help")
+    parser.add_argument("-m", "--model", action="store", required=True, dest="MODEL", choices=['kmeans', 'lda', 'dbscan', 'birch', 'hierarchical'], help="Run model ")
+    parser.add_argument("-r", "--rep",   action="store", required=True, dest="REP", choices=['bow', 'tfidf'], help="Use bag of words representation (BOW), or tfidf representation")
+    args = parser.parse_args()
+
 	# get data
     data_reader = DataReader()
     df = data_reader.get_all_data()
     train_x_raw, train_y_raw, test_x_raw, test_y_raw = get_train_test_split(df)
-    train_y_raw = train_x_raw['RIS PROCEDURE CODE']
-    test_y_raw = test_x_raw['RIS PROCEDURE CODE']
     train_x_raw = pd.concat([train_x_raw, test_x_raw], axis=0)
     train_y_raw = pd.concat([train_y_raw, test_y_raw], axis=0)
-    train_x_raw = train_x_raw.drop("RIS PROCEDURE CODE", axis=1)
-    tokens, train_y_raw = tokenize_columns(train_x_raw, train_y_raw, regex_string=r'[a-zA-Z]+', 
-        save_missing_feature_as_string=False, remove_short=True, remove_num=False, remove_empty=True)
+    train_x_raw.drop(['RIS PROCEDURE CODE'], axis=1, inplace=True)
+    
+    # tokenize and subsample
+    tokens, train_y_raw = tokenize_columns(train_x_raw, train_y_raw, regex_string=r'[a-zA-Z0-9]+', 
+        save_missing_feature_as_string=False, remove_short=True, remove_num=True, remove_empty=True)
+    print("done tokenizing columns")
 
-    # run with regular bag of words
-    train_x, train_y, feature_names = tokens_to_bagofwords(tokens, train_y_raw, CountVectorizer)
-    num_clusters = 1500
-    kmeans = Kmeans(num_clusters, feature_names, train_x)
-    labels = kmeans.labels_
-    sil_score = metrics.silhouette_score(train_x, labels, metric='euclidean')
-    db_idx_score = metrics.davies_bouldin_score(train_x, labels)
-    print(sil_score)
-    print(db_idx_score)
-    print("finished running knn with countvectorizer")
+    # get representation of data
+    feature_names = list()
+    train_x = list()
+    train_y = list()
+    if args.REP == "bow":
+        train_x, train_y, feature_names = tokens_to_bagofwords(tokens, train_y_raw, CountVectorizer)
+        print("done converting to bag of words representation")
+    elif args.REP == "tfidf":
+        train_x, train_y, feature_names = tokens_to_bagofwords(tokens, train_y_raw, TfidfVectorizer)
+        print("done converting to tfidf representation")
 
-    print("getting nearest")
-    kmeans.get_nearest_neighbours("Y DIR - ANGIOGRAM")
+    # run models
+    print(str(args.MODEL) + " results: ")
+    if args.MODEL == "kmeans":
+        num_clusters = 1500
+        kmeans = Kmeans(num_clusters, feature_names, train_x, train_y)
+        kmeans.eval()
+        print("sil score:" + str(kmeans.get_sil_score()))
+        print("db idx:" + str(kmeans.get_db_idx_score()))
+        print("getting nearest: ")
+        kmeans.get_nearest_neighbours("Y DIR - ANGIOGRAM")
+        kmeans.get_nearest_neighbours("US KNEE BIOPSY/ASPIRATION")
+        kmeans.get_nearest_neighbours("G TUBE INSERTION")
+    elif args.MODEL == "lda":
+        # run lda
+        lda = Lda(train_x_raw, train_y_raw, 1500, passes=15)
+        lda.train()
+        print("finished running lda")
+    elif args.MODEL == "dbscan":
+        # run dbscan
+        dbscan = DBscan(10, feature_names, train_x, train_y)
+        dbscan.eval()
+        print("sil score:" + str(dbscan.get_sil_score()))
+        print("db idx:" + str(dbscan.get_db_idx_score()))
+    elif args.MODEL == "birch":
+        b = Birch_(10, feature_names, train_x, train_y)
+        print('sil score: ' + str(b.get_sil_score())) 
+        print('db idx: ' + str(b.get_db_idx_score()))
+    elif args.MODEL == "hierarchical":
+        h = Hierarchial(10, feature_names, train_x, train_y)
+        print('sil score: ' + str(h.get_sil_score())) 
+        print('db idx: ' + str(h.get_db_idx_score()))
 
-    # run with tfidf
-    train_x, train_y, feature_names = tokens_to_bagofwords(tokens, train_y_raw, TfidfVectorizer)
-    num_clusters = 1500
-    kmeans = Kmeans(num_clusters, feature_names, train_x)
-    labels = kmeans.labels_
-    sil_score = metrics.silhouette_score(train_x, labels, metric='euclidean')
-    db_idx_score = metrics.davies_bouldin_score(train_x, labels)
-    print(sil_score)
-    print(db_idx_score)
-    print("finished running knn with tfidfvectorizer")
 
-    # run lda
-    lda = Lda(train_x_raw, train_y_raw, 1500, passes=15)
-    lda.train()
-    print("finished running lda")
 
 if __name__ == '__main__':
     main()
