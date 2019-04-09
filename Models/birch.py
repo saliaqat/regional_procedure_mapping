737193ca8016a7149f817d01e384b6fced38c9f3
jopineda <joannaa.pineda@gmail.com>
2019-04-09 16:26:54 -0400

    inherit attributes and functions from clustermodel parent

--- Models/birch.py
+++ Models/birch.py
@@ -12,6 +12,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from Models.model import Model
+from Models.clustermodel import ClusterModel
 import gensim
 from gensim import corpora
 from gensim.models.coherencemodel import CoherenceModel
@@ -30,93 +31,11 @@ from sklearn.metrics import davies_bouldin_score
 from sklearn.cluster import Birch
 
 
-class Birch_(Model):
-    def __init__(self, num_clusters, feature_names, train_x, train_y):
+class Birch_(ClusterModel):
+    def __init__(self, num_clusters, feature_names, train_x, train_y, rep):
+        ClusterModel.__init__(self, train_x, train_y, feature_names, rep)
         self.birch_model = Birch(n_clusters=num_clusters).fit(train_x)
-        self.feature_names = feature_names
-        self.train_x = train_x
-        self.train_y = train_y
+        self.birch_model.predict(train_x)
         self.labels = self.birch_model.labels_
+        print(self.labels)
         self.num_clusters = num_clusters
-        self.sil_score = -100.0
-        self.db_idx_score = -100.0
-
-    def _tokenize(self, query):
-        regex_string = r'[a-zA-Z]+'
-        tokenizer = RegexpTokenizer(regex_string)
-        tokens = tokenizer.tokenize(query.lower())
-        tokens = [x for x in tokens if x.isalpha()]
-        tokens = [x for x in tokens if len(x) > 2]
-        return tokens
-
-    def _get_cluster_id(self, query):
-        # get the cluster id for a given query
-        # get prediction for a given query
-        tokens = self._tokenize(query)
-
-        # get representation (bag of words)
-        weights, y, _ = tokens_to_bagofwords([tokens, ], 1, CountVectorizer, self.feature_names)
-        pred_y = self.birch_model.predict(weights)
-
-        return pred_y
-
-    def get_nearest_neighbours(self, query):
-        # get cluster id
-        pred_y = self._get_cluster_id(query)
-
-        # tokenize data and get bag of words
-        tokens = self._tokenize(query)
-        weights, y, _ = tokens_to_bagofwords([tokens, ], 1, CountVectorizer, self.feature_names)
-        print(weights)
-
-        # get all training values in same cluster
-        train_x_idx = np.where(self.labels == pred_y)
-        cluster_set = self.train_x[np.where(self.labels == pred_y)]
-        print("Clustered with: " + str(cluster_set.shape[0]))
-
-        # get distances between query and all in same cluster
-        distances = [euclidean_distances(weights, train_example.reshape(1, -1)) for train_example in cluster_set]
-
-        # sort by distance
-        sorted_dist = [d for d, x in sorted(zip(distances, cluster_set), key=lambda x: x[0])]
-        sorted_by_dist = [x for d, x in sorted(zip(distances, cluster_set), key=lambda x: x[0])]
-
-        # print the closest neighbours
-        for i in range(5):
-            print(sorted_by_dist[i])
-            print(np.where(sorted_by_dist[i].toarray() == 1)[1])
-            for x in np.where(cluster_set[i].toarray() != 0)[1]:
-                print(self.feature_names[x])
-
-    def eval(self):
-        self.sil_score = metrics.silhouette_score(self.train_x, self.labels, metric='euclidean')
-        self.db_idx_score = metrics.davies_bouldin_score(self.train_x.toarray(), self.labels)
-        #         print(self.sil_score)
-        #         print(self.db_idx_score)
-
-        # evaluate with ON WG IDENTIFIER
-        self.custom_score()
-
-    def custom_score(self):
-        # evaluate with ON WG IDENTIFIER
-        # how to get the number of ids
-        # for each cluster count the number onwgid occurences
-        # get unique list of cluster ids
-
-        #         unique_cids = self.train_y['ON WG IDENTIFIER'].unique()
-        unique_cids = np.unique(self.train_y)
-        print(len(unique_cids))
-
-    #         print(y)
-    # for cid in unique_cids:
-    # cluster: (onwgid1: 1, onwgid2)
-
-    def get_sil_score(self):
-        #         print(self.train_x)
-        return metrics.silhouette_score(self.train_x, self.labels)
-
-    def get_db_idx_score(self):
-        return davies_bouldin_score(self.train_x, self.labels)
-
-    def get_labels(self):
-        return self.labels
